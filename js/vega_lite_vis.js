1f04ad43a77b126296676bea68e779cc4cfcb0ca
anhtuannee <nguyentrananhtuan2005@gmail.com>
2026-05-12 23:39:05 +1000

    build chart 4

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,6 +1,7 @@
 const priceChart = "charts/fuel_prices_over_time.vg.json";
 const stateSalesChart = "charts/fuel_sales_by_state.vg.json";
 const stateFuelTypeChart = "charts/state_fuel_type_comparison.vg.json";
+const productMixChart = "charts/product_mix_over_time.vg.json";
 
 vegaEmbed("#fuel-prices-chart", priceChart, {
   actions: false
@@ -11,5 +12,9 @@ vegaEmbed("#state-sales-chart", stateSalesChart, {
 }).catch(console.error);
 
 vegaEmbed("#state-fuel-type-chart", stateFuelTypeChart, {
+  actions: false
+}).catch(console.error);
+
+vegaEmbed("#product-mix-chart", productMixChart, {
   actions: false
 }).catch(console.error);
