97845609e8874a2a61d2abe05efbdefcb2cdcd95
anhtuannee <nguyentrananhtuan2005@gmail.com>
2026-05-13 15:52:22 +1000

    build chart 5 and 6 and consistently debug the error while build the Choropleth map

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -2,6 +2,8 @@ const priceChart = "charts/fuel_prices_over_time.vg.json";
 const stateSalesChart = "charts/fuel_sales_by_state.vg.json";
 const stateFuelTypeChart = "charts/state_fuel_type_comparison.vg.json";
 const productMixChart = "charts/product_mix_over_time.vg.json";
+const supplyDependenceChart = "charts/supply_dependence.vg.json";
+const importSourcesMap = "charts/import_sources_map.vg.json";
 
 vegaEmbed("#fuel-prices-chart", priceChart, {
   actions: false
@@ -16,5 +18,13 @@ vegaEmbed("#state-fuel-type-chart", stateFuelTypeChart, {
 }).catch(console.error);
 
 vegaEmbed("#product-mix-chart", productMixChart, {
+  actions: false
+}).catch(console.error);
+
+vegaEmbed("#supply-dependence-chart", supplyDependenceChart, {
+  actions: false
+}).catch(console.error);
+
+vegaEmbed("#import-sources-map", importSourcesMap, {
   actions: false
 }).catch(console.error);
